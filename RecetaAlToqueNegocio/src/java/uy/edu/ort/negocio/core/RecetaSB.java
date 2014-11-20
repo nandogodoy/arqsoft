8d926ba2876b2d8768e59f864b5084dd4379cba8
Richard <Richard@Richard-PC>
2014-11-20 20:27:37 -0200

    Merge origin/master
    Conflicts:
    	RecetaAlToque-ejb/nbproject/project.properties
    	RecetaAlToque-ejb/setup/glassfish-resources.xml
    	RecetaAlToque-war/nbproject/build-impl.xml~

---
+++ RecetaAlToqueNegocio/src/java/uy/edu/ort/negocio/core/RecetaSB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uy.edu.ort.negocio.core;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Richard
+ */
+@Stateless
+public class RecetaSB implements RecetaSBLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ RecetaAlToqueNegocio/src/java/uy/edu/ort/negocio/gestion/IngredienteSB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uy.edu.ort.negocio.gestion;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Richard
+ */
+@Stateless
+public class IngredienteSB implements IngredienteSBLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
